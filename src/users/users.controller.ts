a6900e23ab827bf3c7db7ad916f0619b12d87705
Omid Badkoubeh <omid.badkoubeh@youtopin.com>
2021-09-15 23:37:25 +0430

    feat: createUser

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Post('signup')
   async createUser(@Body() body: CreateUserDto) {
-    console.log(body);
-    // return await this.userService.create()
+    return await this.usersService.create(body.email, body.password);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly userRepo: Repository<User>,
+  ) {}
+
+  async create(email: string, password: string): Promise<User> {
+    const user = this.userRepo.create({ email, password });
+
+    return this.userRepo.save(user);
+  }
+}
