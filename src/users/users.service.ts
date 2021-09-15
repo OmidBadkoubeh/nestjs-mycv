75b57eae8297d4886f46d9e2f696e85c087ca8cc
Omid Badkoubeh <omid.badkoubeh@youtopin.com>
2021-09-15 23:54:31 +0430

    feat: complete userService crud

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,4 +14,29 @@ export class UsersService {
 
     return this.userRepo.save(user);
   }
+
+  async findOne(id: string): Promise<User> {
+    return this.userRepo.findOne(id);
+  }
+
+  async find(email: string): Promise<User[]> {
+    return this.userRepo.find({ email });
+  }
+
+  async update(id: string, attrs: Partial<User>): Promise<User> {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    Object.assign(user, attrs);
+    return this.userRepo.save(user);
+  }
+
+  async remove(id: string): Promise<User> {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return this.userRepo.remove(user);
+  }
 }
