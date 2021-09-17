d1b5ec22e3e8c38774dde475069cbbfa065ea180
Omid Badkoubeh <omid.badkoubeh@gmail.com>
2021-09-17 23:17:47 +0430

    fix: reorder middlewares to fix cookie issue

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(
     cookieSession({
       keys: ['secretCookie'],
     }),
   );
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
 bootstrap();
