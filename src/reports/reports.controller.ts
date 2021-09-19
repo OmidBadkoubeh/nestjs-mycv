b4c0e2f822588f3b844957307aabceba5a979cbc
Omid Badkoubeh <omid.badkoubeh@gmail.com>
2021-09-19 23:35:28 +0430

    feat: add association & serialize output

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { CurrentUser, Serialize } from '@src/decorators';
 import { AuthGuard } from '@src/guards';
-import { CreateReportDto } from './dtos';
+import { User } from '@src/users/user.entity';
+import { CreateReportDto, ReportDto } from './dtos';
 import { ReportsService } from './reports.service';
 
 @Controller('reports')
@@ -8,8 +10,9 @@ export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
 
   @Post()
+  @Serialize(ReportDto)
   @UseGuards(AuthGuard)
-  async createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body);
+  async createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
   }
 }
