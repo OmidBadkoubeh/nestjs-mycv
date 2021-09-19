0c2ec95366f2db93728dc588062fa75af83a0fa3
Omid Badkoubeh <omid.badkoubeh@gmail.com>
2021-09-19 19:55:38 +0430

    feat: move global pipes & middlewares to appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
-import { Module } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session');
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -19,6 +22,13 @@ import { Report } from './reports/report.entity';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_PIPE, useValue: new ValidationPipe({ whitelist: true }) },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cookieSession({ keys: ['secretCookie'] })).forRoutes('*');
+  }
+}
