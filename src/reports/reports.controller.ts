0aff61be4b444530065488b2ac95433e32362ae9
Omid Badkoubeh <omid.badkoubeh@gmail.com>
2021-09-19 23:53:37 +0430

    feat: updateApproval of report functionality

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,8 +1,15 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CurrentUser, Serialize } from '@src/decorators';
 import { AuthGuard } from '@src/guards';
 import { User } from '@src/users/user.entity';
-import { CreateReportDto, ReportDto } from './dtos';
+import { ApproveReportDto, CreateReportDto, ReportDto } from './dtos';
 import { ReportsService } from './reports.service';
 
 @Controller('reports')
@@ -14,5 +21,11 @@ export class ReportsController {
   @UseGuards(AuthGuard)
   async createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
+  }
+
+  @Patch(':id')
+  async approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    const { approved } = body;
+    return this.reportsService.changeApproval(+id, approved);
   }
 }
